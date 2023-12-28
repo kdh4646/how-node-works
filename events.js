40c50480910386c19d9478eaabf2059cb39f8047
Derek Kweon <kdh4646@hanmail.net>
2023-12-28 15:49:31 -0800

    2023-12-28 complete events.js, make streams.js

--- events.js
+++ events.js
@@ -1,17 +1,51 @@
 const EventEmitter = require("events");
+const http = require("http");
 
-const myEmitter = new EventEmitter();
+class Sales extends EventEmitter {
+  constructor() {
+    super();
+  }
+}
+
+const myEmitter = new Sales();
 
 /* Observer Pattern */
 
 //Observer1
-myEmitter.on("newsale", () => {
+myEmitter.on("newSale", () => {
   console.log("There was a new sale!");
 });
 
 //Observer2
-myEmitter.on("newsale", () => {
+myEmitter.on("newSale", () => {
   console.log("Costumer name: Jonas");
 });
 
+//Observer3
+myEmitter.on("newSale", (stock) => {
+  console.log(`There are now ${stock} items left in stock.`);
+});
+
 myEmitter.emit("newSale");
+
+//===============================//
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  console.log("Request received!");
+  console.log(req.url);
+  res.end("Request received");
+});
+
+server.on("request", (req, res) => {
+  console.log("Another request 👍");
+});
+
+server.on("close", () => {
+  console.log("Server closed");
+});
+
+server.listen(8000, "127.0.0.1", () => {
+  console.log("Waiting for requests...");
+});
